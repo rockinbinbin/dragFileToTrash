ababc29c63b18bb9b4a5b8dc65e1652d6e8ff694
Robin <robimeht@umich.edu>
2016-08-16 21:09:08 -0700

    Initial Commit

---
+++ dragFileToTrash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dragFileToTrash
+//
+//  Created by Robin Mehta on 8/16/16.
+//  Copyright © 2016 robin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
